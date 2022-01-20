11097712d5a9685dc132a82fad2822fabbda6e7e
JoaoPedroCorreia1 <jpmourasilva@gmail.com>
2022-01-20 17:44:39 -0300

    genesis 1.0

---
+++ controller/GameController.js
@@ -0,0 +1,158 @@
+class GameController {
+
+    // inicialization
+    constructor()
+    {
+        this.order = []
+        this.clickedOrder = [];
+
+        this.score = 0;
+
+        this.buttons = this.createButtons();
+
+        this.backgroundAudio = document.getElementById('background-audio');
+
+        this.congratulationsAudio = document.getElementById('congratulations-audio');
+        this.mistakeAudio = document.getElementById('mistake-audio');
+    }
+
+    createButtons()
+    {
+        return [
+        new Button(
+            this,
+            document.querySelector('.blue'),
+            0,
+            document.getElementById('bluebutton-audio')
+        ),
+        
+        new Button(
+            this,
+            document.querySelector('.red'),
+            1,
+            document.getElementById('redbutton-audio')
+        ),
+
+        new Button(
+            this,
+            document.querySelector('.green'),
+            2,
+            document.getElementById('greenbutton-audio')
+        ),
+        
+        new Button(
+            this,
+            document.querySelector('.yellow'),
+            3,
+            document.getElementById('yellowbutton-audio')
+        )];
+    }
+    
+
+    // events
+    eventButtonSelected(colorNumber)
+    {
+        this.clickedOrder.push(colorNumber);
+        this.checkOrder();
+    }
+
+    // public functions
+    playGame ()
+    {
+        alert('Bem vindo ao Gênesis! Iniciando novo jogo!');
+        this.score = 0;
+    
+        this.nextLevel();
+    }
+    
+    // private functions
+    gameOver()
+    {
+        this.mistakeAudio.play();
+
+        alert(
+            `Pontuação: ${this.score}!
+            \nVocê perdeu o jogo!
+            \nClique em OK para iniciar um novo jogo`);
+        this.order = [];
+        this.clickedOrder = [];
+        
+        this.playGame();
+    }
+
+
+    nextLevel()     
+    {
+        this.score++;
+        this.clickedOrder = [];
+
+        setTimeout(() => {
+        this.shuffleOrder();
+        },
+        1000);
+    }
+
+    checkOrder()
+    {
+        for(let button in this.clickedOrder) 
+        {
+            if(this.clickedOrder[button] != this.order[button]) 
+            {
+                this.gameOver();
+                break;
+            }
+        }
+
+        if(this.clickedOrder.length === this.order.length) 
+        {
+            this.congratulationsAudio.play();
+            alert(`Pontuação: ${this.score}\nVocê acertou! Iniciando próximo nível!`);
+            this.nextLevel();
+        }
+    }
+
+    shuffleOrder()
+    {
+        let newColor = Math.floor(Math.random() * 4);
+        let newColorIndex = this.order.length;
+        
+        this.order[newColorIndex] = newColor;
+
+        this.lockButtons();
+
+        console.log(this.order);
+        for(let i in this.order) 
+        {
+            let colorNumber = this.order[Number(i)];
+
+            let button = this.buttons[colorNumber];
+            button.lightColor(Number(i) + 1);
+        }
+
+        setTimeout(() => {
+           this.unlockButtons(); 
+        }, 
+        this.order.length * 800 + 800);
+    }
+
+    lockButtons()
+    {
+        for(let i in this.buttons)
+        {
+            let button = this.buttons[Number(i)];
+
+            button.setLocked(true);
+        }
+    }
+
+    unlockButtons()
+    {
+        for(let i in this.buttons)
+        {
+            let button = this.buttons[Number(i)];
+
+            button.setLocked(false);
+        }
+    }
+
+}
---
+++ models/Button.js
@@ -0,0 +1,82 @@
+class Button {
+
+    constructor(
+        gameController,
+        element,
+        colorNumber,
+        clickAudio
+    ) {
+        // scripts
+        this.gameController = gameController
+
+        // variables
+        this.element = element;
+        this.colorNumber = colorNumber;
+
+        this.clickAudio = clickAudio;
+
+        this.isInAnimation = false;
+
+        this.locked = false;
+    }
+
+    // getters and setters
+    setLocked(value)
+    {
+        this.locked = value;
+    }
+    
+    // public functions
+    onClick()
+    {
+        if(this.isInAnimation === false
+        && this.locked === false)
+        {
+            this.clickAudio.pause();
+            this.clickAudio.currentTime = 0;
+
+            this.clickAudio.play();
+
+            this.select();
+            this.gameController
+                .eventButtonSelected(this.colorNumber);
+        }
+    }
+
+    // private functions
+    select()
+    {
+        this.isInAnimation = true;
+
+        this.element.classList.add('selected');
+
+        setTimeout(() => {
+            this.isInAnimation = false;
+
+            this.element.classList.remove('selected');
+        }, 
+        500);
+    }
+
+    lightColor(number)
+    {
+        
+
+        number = number * 800;
+
+        setTimeout(() => {
+            this.clickAudio.pause();
+            this.clickAudio.currentTime = 0;
+
+            this.clickAudio.play();
+            this.element.classList.add('selected');
+        }, 
+        number - 800);
+
+        setTimeout(() => {
+            this.element.classList.remove('selected');
+        }, 
+        number - 150);
+    }
+
+}
